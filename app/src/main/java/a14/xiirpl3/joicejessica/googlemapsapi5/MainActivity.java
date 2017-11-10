6b52f4033edb1369591b06225970e8273d5d90eb
Joice Jessica <joicejssc@gmail.com>
2017-11-10 14:27:43 +0700

    Add Street View

--- app/src/main/java/a14/xiirpl3/joicejessica/googlemapsapi5/MainActivity.java
+++ app/src/main/java/a14/xiirpl3/joicejessica/googlemapsapi5/MainActivity.java
@@ -3,11 +3,30 @@ package a14.xiirpl3.joicejessica.googlemapsapi5;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.OnStreetViewPanoramaReadyCallback;
+import com.google.android.gms.maps.StreetViewPanorama;
+import com.google.android.gms.maps.StreetViewPanoramaFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.StreetViewPanoramaCamera;
+
+public class MainActivity extends AppCompatActivity implements OnStreetViewPanoramaReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        StreetViewPanoramaFragment streetViewPanoramaFragment = (StreetViewPanoramaFragment) getFragmentManager().findFragmentById(R.id.panorama);
+        streetViewPanoramaFragment.getStreetViewPanoramaAsync(this);
+
+    }
+
+    @Override
+    public void onStreetViewPanoramaReady(StreetViewPanorama panorama) {
+        panorama.setPosition(new LatLng(-6.921932, 107.607639));
+        StreetViewPanoramaCamera camera = new StreetViewPanoramaCamera.Builder()
+                .bearing(180)
+                .build();
+        panorama.animateTo(camera, 10000);
     }
 }
